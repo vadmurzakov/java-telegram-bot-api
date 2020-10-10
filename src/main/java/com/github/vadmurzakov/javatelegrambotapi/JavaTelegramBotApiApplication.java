492b7c024a03e7736b643f7fb9cad76f0490a503
vmurzakov <vadmurzakov@gmail.com>
2020-10-10 23:22:27 +0300

    [GH-1] initial repo

---
+++ src/main/java/com/github/vadmurzakov/javatelegrambotapi/JavaTelegramBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.vadmurzakov.javatelegrambotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramBotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTelegramBotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/vadmurzakov/javatelegrambotapi/JavaTelegramBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vadmurzakov.javatelegrambotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTelegramBotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
