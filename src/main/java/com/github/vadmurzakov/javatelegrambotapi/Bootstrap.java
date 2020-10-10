93e8d10ccf99df477864121a0891f056c0334014
vmurzakov <vadmurzakov@gmail.com>
2020-10-10 23:33:14 +0300

    [GH-2] rename main class on Bootstrap.java

--- src/main/java/com/github/vadmurzakov/javatelegrambotapi/Bootstrap.java
+++ src/main/java/com/github/vadmurzakov/javatelegrambotapi/Bootstrap.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaTelegramBotApiApplication {
+public class Bootstrap {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaTelegramBotApiApplication.class, args);
+        SpringApplication.run(Bootstrap.class, args);
     }
 
 }
